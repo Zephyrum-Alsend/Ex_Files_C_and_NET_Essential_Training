2c7348a8bf04a4b14f4181eb196468412d2e72cc
Zephyrum Alsend <isfaryl@gmail.com>
2022-10-17 22:49:34 -0400

    Completed course
    I may return to properly implement the REGEX challenge after I learn more about REGEX. Otherwise all exercises are done.

---
+++ Exercise Files/Start/Regex/Challenge/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// After peeking the solution, I know nowhere near enough REGEX to implement this at the moment.
+// Rather than have nothing, here's an outline of what was my plan:
+/*
+Take input
+if (DateTime.TryParse(userinput)) {
+    Know it's MM/DD/YYYY format
+    Scan for the last 4 digits, move them to the front
+}
+else {
+    cw("Not a valid date!");
+}
+*/
+// This could be implemented without REGEX using string parsing or DateTime local cultures.
